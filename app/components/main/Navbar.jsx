c8853be3f0e2d1fbc2edc331995591c3bd477814
killermax <thekillermax1999@gmail.com>
2023-10-22 10:19:04 +0530

    transparent sticy navbar

--- app/components/main/Navbar.jsx
+++ app/components/main/Navbar.jsx
@@ -3,18 +3,18 @@
 const Navbar = () => {
   return (
     <>
-      <div className="flex bg-black text-white h-16 ">
+      <div className="flex bg-black/70 text-white h-16 fixed w-full z-20 backdrop-blur-md">
         <div className="flex items-center w-1/5 uppercase font-semibold text-2xl px-6">
           <span className="cursor-pointer">markall</span>
         </div>
         <div className="w-3/5 flex justify-center space-x-2 items-center text-lg">
-          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-neutral-500 hover:text-white hover:bg-neutral-800 px-6 py-2 rounded-md">
+          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-white/50 hover:text-white hover:bg-neutral-500/20 px-6 py-2 rounded-md">
             services
           </div>
-          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-neutral-500 hover:text-white hover:bg-neutral-800 px-6 py-2 rounded-md">
+          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-white/50 hover:text-white hover:bg-neutral-500/20 px-6 py-2 rounded-md">
             prices
           </div>
-          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-neutral-500 hover:text-white hover:bg-neutral-800 px-6 py-2 rounded-md">
+          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-white/50 hover:text-white hover:bg-neutral-500/20 px-6 py-2 rounded-md">
             about us
           </div>
         </div>
