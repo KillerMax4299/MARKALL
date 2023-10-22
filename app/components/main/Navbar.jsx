c416634d635475a6aa7d0c0dad6ed6aa49794de1
killermax <thekillermax1999@gmail.com>
2023-10-22 10:37:30 +0530

    navbar elements created

--- app/components/main/Navbar.jsx
+++ app/components/main/Navbar.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 const Navbar = () => {
   return (
@@ -8,15 +8,9 @@ const Navbar = () => {
           <span className="cursor-pointer">markall</span>
         </div>
         <div className="w-3/5 flex justify-center space-x-2 items-center text-lg">
-          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-white/50 hover:text-white hover:bg-neutral-500/20 px-6 py-2 rounded-md">
-            services
-          </div>
-          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-white/50 hover:text-white hover:bg-neutral-500/20 px-6 py-2 rounded-md">
-            prices
-          </div>
-          <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-white/50 hover:text-white hover:bg-neutral-500/20 px-6 py-2 rounded-md">
-            about us
-          </div>
+          <Elements text="services" />
+          <Elements text="prices" />
+          <Elements text="about us" />
         </div>
         <div className="w-1/5 flex justify-end items-center px-4">
           <button className="h-fit font-semibold text-md py-[2px] transition-all duration-200 active:scale-95 hover:text-green-300 hover:bg-black capitalize rounded-md text-black border-2 border-green-400 bg-green-400 w-fit px-6">
@@ -26,5 +20,13 @@ const Navbar = () => {
       </div>
     </>
   );
-}
-export default Navbar
+};
+export default Navbar;
+
+const Elements = ({ text }) => {
+  return (
+    <div className="transition-all duration-300 cursor-pointer capitalize w-fit text-white/50 hover:text-white hover:bg-neutral-500/20 px-6 py-2 rounded-md">
+      {text}
+    </div>
+  );
+};
